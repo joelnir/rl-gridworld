bdd47e1924fa5636be6c42f11fabd10dd692d255
joeloskarsson <joel.oskarsson@outlook.com>
2019-10-28 11:52:41 +0100

    Make greedy policy sample actions uniformly from the argmax set

--- src/policy.py
+++ src/policy.py
@@ -6,7 +6,16 @@ def random_policy(grid_world):
 
 def greedy_policy(grid_world):
     pos = grid_world.get_position()
-    return np.argmax(grid_world.q[pos[0], pos[1]])
+
+    # Sample uniformly from  argmax set
+    Q_s = grid_world.q[pos[0], pos[1]]
+    argmax = np.argwhere(Q_s == np.amax(Q_s))[:,0]
+    argmax_size = argmax.shape[0]
+
+    if argmax_size == 1:
+        return argmax[0]
+    else:
+        return argmax[random.randint(0,argmax_size-1)]
 
 def eps_greedy_policy(grid_world):
     if random.random() > grid_world.epsilon:
